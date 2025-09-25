f5973c2dfe97bed84c7314613e5760859ab56180
rkhue <85773727+rkhue@users.noreply.github.com>
2025-09-25 12:24:41 -0300

    (feat) Criando controller do AliveController

---
+++ src/main/java/org/purpura/apipg/controller/alive/AliveController.java
@@ -0,0 +1,20 @@
+package org.purpura.apipg.controller.alive;
+
+import org.purpura.apipg.dto.schemas.alive.AliveResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/alive")
+public class AliveController {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public AliveResponseDTO alive() {
+        return ResponseEntity.ok(new AliveResponseDTO("banana", "Api is alive!"));
+    }
+}
