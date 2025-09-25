42ee36ffa9c2273c1e2b6037e09f5dfe3c9df57d
rkhue <85773727+rkhue@users.noreply.github.com>
2025-09-24 21:20:50 -0300

    (feat) Primeiro commit

---
+++ src/main/java/org/purpura/apipg/ApiPgApplication.java
@@ -0,0 +1,13 @@
+package org.purpura.apipg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/purpura/apipg/ApiPgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.purpura.apipg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
