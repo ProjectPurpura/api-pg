4bb077478d22d445c51f3ec2a2c322f562d0d7ed
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-03 07:17:48 -0300

    (feat) Ligando state do pedido à model

--- src/main/java/org/purpura/apipg/model/pedido/PedidoModel.java
+++ src/main/java/org/purpura/apipg/model/pedido/PedidoModel.java
@@ -5,8 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
+import org.purpura.apipg.model.pedido.state.*;
 
 @Data
 @NoArgsConstructor
@@ -18,18 +17,54 @@ public class PedidoModel {
     @Id
     Long idPedido;
 
-    @Builder.Default
-    long data = System.currentTimeMillis();
+    Long agendamentoColeta;
 
-    LocalDate agendamentoColeta;
+
+    @Column(name="fk_recebedor")
+    String idRecebedor;
+
+    @Column(name="fk_entregador")
+    String idEntregador;
+
+    @Builder.Default
+    Long data = System.currentTimeMillis();
+
+    @Builder.Default
+    Double valorTotal = 0.0;
+
+    @Builder.Default
+    String observacoes = "";
 
     @Enumerated(EnumType.STRING)
     PedidoStatus status;
 
-    @Builder.Default
-    String observacoes = "";
-    Double valorTotal;
+    @Transient
+    private transient PedidoState state;
 
-    String fkRecebedor;
-    String fkEntregador;
+    public PedidoState getState() {
+        if (state == null) {
+            switch (status) {
+                case PENDENTE -> state = new PendenteState();
+                case APROVADO -> state = new AprovadoState();
+                case CONCLUIDO -> state = new ConcluidoState();
+                case CANCELADO -> state = new CanceladoState();
+            }
+        }
+        return state;
+    }
+
+    public void aprovar() {
+        this.state = getState().aprovar();
+        this.status = state.getStatus();
+    }
+
+    public void cancelar() {
+        this.state = getState().cancelar();
+        this.status = state.getStatus();
+    }
+
+    public void concluir() {
+        this.state = getState().concluir();
+        this.status = state.getStatus();
+    }
 }
