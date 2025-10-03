1ba0ebffaa7b8d3d843eb800efa129e4da019f2d
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-03 12:16:54 -0300

    (refactor) Simplificando nomes de métodos

--- src/main/java/org/purpura/apipg/repository/pedido/PedidoRepository.java
+++ src/main/java/org/purpura/apipg/repository/pedido/PedidoRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
 @Repository
 public interface PedidoRepository extends JpaRepository<PedidoModel, Long> {
     @Query(value = "SELECT * FROM pedido WHERE pedido.fk_recebedor = ?1", nativeQuery = true)
-    List<PedidoModel> findAllByfkRecebedor(String fkRecebedor);
+    List<PedidoModel> findAllByRecebedor(String fkRecebedor);
 
     @Query(value = "SELECT * FROM pedido WHERE pedido.fk_entregador = ?1", nativeQuery = true)
-    List<PedidoModel> findAllByfkEntregador(String fkRecebedor);
+    List<PedidoModel> findAllByEntregador(String fkRecebedor);
 
 }
