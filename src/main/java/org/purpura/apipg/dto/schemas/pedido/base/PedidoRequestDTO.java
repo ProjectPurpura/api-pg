5f705e0b199a18de3964d90928ac206906de4d8f
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-03 07:20:58 -0300

    (feat) DTOs de resposta de pedido

---
+++ src/main/java/org/purpura/apipg/dto/schemas/pedido/base/PedidoRequestDTO.java
@@ -0,0 +1,28 @@
+package org.purpura.apipg.dto.schemas.pedido;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.purpura.apipg.model.pedido.PedidoStatus;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Schema(name = "PedidoRequestDTO", description = "DTO para a inserção/atualização de um pedido")
+public class PedidoRequestDTO {
+    @Schema(description = "Data de agendamento da coleta (timestamp)", example = "1627702400000")
+    @Builder.Default
+    private Long agendamentoColeta = null;
+
+    @Schema(description = "Data do pedido (timestamp)", example = "1627702400000")
+    @Builder.Default
+    private Long data = System.currentTimeMillis();
+
+    @Schema(description = "Status do pedido", example = "PENDENTE")
+    @Builder.Default
+    private PedidoStatus status = PedidoStatus.PENDENTE;
+}
---
+++ src/main/java/org/purpura/apipg/dto/schemas/pedido/base/PedidoResponseDTO.java
@@ -0,0 +1,27 @@
+package org.purpura.apipg.dto.schemas.pedido;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.purpura.apipg.model.pedido.PedidoStatus;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Schema(name = "PedidoResponseDTO", description = "Representa um DTO de resposta a um pedido retornado da API")
+public class PedidoResponseDTO {
+    @Schema(description = "ID do pedido", example = "1")
+    private Long id;
+
+    @Schema(description = "Data de agendamento da coleta (timestamp)", example = "1627702400000")
+    private Long agendamentoColeta;
+
+    @Schema(description = "Data do pedido (timestamp)", example = "1627702400000")
+    private Long data = System.currentTimeMillis();
+
+    @Schema(description = "Status do pedido", example = "PENDENTE")
+    private PedidoStatus status;
+}
