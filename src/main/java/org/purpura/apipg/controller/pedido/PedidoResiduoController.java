177d0081961fcf691044a326480773002620ce7f
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-06 19:33:27 -0300

    (feat) Separando o Contract do controller PedidoResiduoController

--- src/main/java/org/purpura/apipg/controller/pedido/PedidoResiduoController.java
+++ src/main/java/org/purpura/apipg/controller/pedido/PedidoResiduoController.java
@@ -2,6 +2,7 @@ package org.purpura.apipg.controller.pedido;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.purpura.apipg.controller.pedido.oas.PedidoResiduoContract;
 import org.purpura.apipg.dto.schemas.pedido.base.PedidoResiduoRequestDTO;
 import org.purpura.apipg.dto.schemas.pedido.base.PedidoResiduoResponseDTO;
 import org.purpura.apipg.service.pedido.PedidoResiduoService;
@@ -14,23 +15,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/pedido/{pedidoId}/residuo")
 @RequiredArgsConstructor
-public class PedidoResiduoController {
+public class PedidoResiduoController implements PedidoResiduoContract {
 
     private final PedidoResiduoService pedidoResiduoService;
     private final PedidoService pedidoService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     public PedidoResiduoResponseDTO addResiduo(
             @PathVariable Long pedidoId,
-            @RequestBody @Valid
             PedidoResiduoRequestDTO pedidoResiduoRequestDTO
     ) {
         return pedidoService.addResiduo(pedidoId, pedidoResiduoRequestDTO);
     }
 
-    @GetMapping("/all")
-    @ResponseStatus(HttpStatus.OK)
     public List<PedidoResiduoResponseDTO> getResiduos(@PathVariable Long pedidoId) {
         return pedidoResiduoService.getResiduosByPedido(pedidoId);
     }
