fc1c298ec63549ea357121dc8fdd3d9107fb7e2a
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-08 13:10:26 -0300

    (doc) Remove comentário do construtor da exceção PagamentoNotFoundException

--- src/main/java/org/purpura/apipg/service/pedido/pagamento/PagamentoNotFoundException.java
+++ src/main/java/org/purpura/apipg/service/pedido/pagamento/PagamentoNotFoundException.java
@@ -4,11 +4,6 @@ import org.purpura.apipg.exception.base.NotFoundException;
 
 
 public class PagamentoNotFoundException extends NotFoundException {
-
-    /**
-     * Constructor.
-     * @param id
-     */
     public PagamentoNotFoundException(Long id) {
         super(String.format("Pagamento com id %d não encontrado", id));
     }
