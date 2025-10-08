0e20ceb350689912ee915fb2c06858786766264c
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-08 12:05:46 -0300

    (feat) Fazendo o PagamentoMapper preencher o pedidoId

--- src/main/java/org/purpura/apipg/dto/mapper/pedido/PagamentoMapper.java
+++ src/main/java/org/purpura/apipg/dto/mapper/pedido/PagamentoMapper.java
@@ -4,11 +4,24 @@ import org.purpura.apipg.dto.mapper.base.BeanUtilMapper;
 import org.purpura.apipg.dto.schemas.pedido.pagamento.PagamentoRequestDTO;
 import org.purpura.apipg.dto.schemas.pedido.pagamento.PagamentoResponseDTO;
 import org.purpura.apipg.model.pedido.pagamento.PagamentoModel;
+import org.purpura.apipg.service.pedido.base.PedidoService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PagamentoMapper extends BeanUtilMapper<PagamentoModel, PagamentoRequestDTO, PagamentoResponseDTO> {
-    public PagamentoMapper() {
+    private final PedidoService pedidoService;
+
+    public PagamentoMapper(PedidoService pedidoService) {
         super(PagamentoModel.class, PagamentoResponseDTO.class);
+        this.pedidoService = pedidoService;
+    }
+
+    @Override
+    public PagamentoModel toModel(PagamentoRequestDTO dto) {
+        PagamentoModel model = super.toModel(dto);
+        if (dto.getIdPedido() != null) {
+            model.setPedido(pedidoService.findById(dto.getIdPedido()));
+        }
+        return model;
     }
 }
