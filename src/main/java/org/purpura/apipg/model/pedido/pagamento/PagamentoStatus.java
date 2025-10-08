fe546d68c371abfe68c59de38d344c15bef7af31
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-08 09:22:30 -0300

    (feat) Refatoração das classes PagamentoModel, PagamentoStatus e PagamentoTipo para incluir conversores e melhorar a representação dos enums

--- src/main/java/org/purpura/apipg/model/pedido/pagamento/PagamentoStatus.java
+++ src/main/java/org/purpura/apipg/model/pedido/pagamento/PagamentoStatus.java
@@ -1,8 +1,34 @@
 package org.purpura.apipg.model.pedido.pagamento;
 
-public enum PagamentoStatus {
-    PENDENTE,
-    CONCLUIDO,
-    CANCELADO
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.purpura.apipg.util.enums.GenericEnumConverter;
+import org.purpura.apipg.util.enums.ValuedEnum;
+
+public enum PagamentoStatus implements ValuedEnum<String> {
+    PENDENTE("pendente"),
+    CONCLUIDO("concluído"),
+    CANCELADO("cancelado");
+
+    private final String value;
+
+    PagamentoStatus(String value) { this.value = value; }
+
+    @Override
+    @JsonValue
+    public String getValue() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static PagamentoStatus fromValue(String value) {
+        return GenericEnumConverter.fromValue(PagamentoStatus.class, value);
+    }
+
+    public static class Converter extends GenericEnumConverter<PagamentoStatus, String> {
+        public Converter() {
+            super(PagamentoStatus.class);
+        }
+    }
 }
 
