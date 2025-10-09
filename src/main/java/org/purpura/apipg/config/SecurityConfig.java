972295eea56d4759265d519c040bfb3454045b5b
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-09 14:56:42 -0300

    (feat) Melhorando o spring security para proteger melhor os headers e contra iframe malicioso

--- src/main/java/org/purpura/apipg/config/SecurityConfig.java
+++ src/main/java/org/purpura/apipg/config/SecurityConfig.java
@@ -4,16 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .authorizeHttpRequests(authz -> authz
-                        .anyRequest().permitAll()
-                );
+                .csrf().and()
+                .headers(headers -> headers
+                        .contentSecurityPolicy(csp -> csp.policyDirectives(
+                                "default-src 'self'; script-src 'self' 'nonce-<RANDOM-NONCE>'; object-src 'none';"
+                        ))
+                        .frameOptions(frame -> frame.sameOrigin())
+                        .contentTypeOptions(withDefaults())
+                        .referrerPolicy(policy -> policy.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.NO_REFERRER))
+                )
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 }
