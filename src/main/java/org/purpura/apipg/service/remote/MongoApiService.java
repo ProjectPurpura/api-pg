28deec89c6b94a37879fba135daa285bdc636f9e
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-30 12:11:33 -0300

    (feat) Criando service de integração com api do mongo

---
+++ src/main/java/org/purpura/apipg/service/remote/MongoApiService.java
@@ -0,0 +1,47 @@
+package org.purpura.apipg.service.remote;
+
+import jakarta.annotation.PostConstruct;
+import org.purpura.apipg.exception.remote.ResiduoInsufficientStockException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class MongoApiService {
+    private final WebClient.Builder webClientBuilder;
+    private WebClient webClient;
+
+    @Value("${mongo.api.base-url:http://localhost:8081}")
+    private String baseUrl;
+
+    public MongoApiService(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
+
+    @PostConstruct
+    private void initWebClient() {
+        this.webClient = webClientBuilder
+                .baseUrl(baseUrl)
+                .build();
+    }
+
+
+    public void downturnStock(String cnpj, String residuoId, Long quantity) {
+        webClient.post()
+                .uri("/empresa/{cnpj}/residuo/{residuoId}/downturn", cnpj, residuoId)
+                .attribute("quantity", quantity)
+                .retrieve()
+                .onStatus(
+                        HttpStatusCode::is4xxClientError,
+                        clientResponse -> {
+                            throw new ResiduoInsufficientStockException(clientResponse.bodyToMono(String.class).block());
+                        })
+                .onStatus(HttpStatusCode::is2xxSuccessful,
+                        clientResponse -> {
+                            clientResponse.bodyToMono(String.class).subscribe();
+                            return null;
+                        });
+    }
+
+}
