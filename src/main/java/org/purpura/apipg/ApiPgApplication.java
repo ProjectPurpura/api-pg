91eba1c018b207af90ccf3750f9c64610563770e
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-30 12:08:52 -0300

    (feat) Add MongoApiProperties  para integração com a api de mongo

--- src/main/java/org/purpura/apipg/ApiPgApplication.java
+++ src/main/java/org/purpura/apipg/ApiPgApplication.java
@@ -1,8 +1,11 @@
 package org.purpura.apipg;
 
+import org.purpura.apipg.config.MongoApiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(MongoApiProperties.class)
 @SpringBootApplication
 public class ApiPgApplication {
 
